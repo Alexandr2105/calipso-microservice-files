a619403f797ce4609ac4e3cfa8306dbcf935ffbc
Alexandr2105 <5030553@gmail.com>
2023-07-20 18:48:09 +0300

    test delete cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'https://example.com' });
   await app.listen(3001);
   // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
   //   AppModule,
