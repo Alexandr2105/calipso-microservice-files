b2d51dd7cb4ddf4644f2b5df9f1864d3501b6682
Alexandr2105 <5030553@gmail.com>
2023-07-20 18:32:57 +0300

    test add enableCors 1

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3001' });
+  app.enableCors({ origin: 'https://alypso-git-dev-alexandr2105.vercel.app' });
   await app.listen(3001);
   // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
   //   AppModule,
