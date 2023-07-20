3e96b406db4e96211dea505c202250f0ed931dbb
Alexandr2105 <5030553@gmail.com>
2023-07-20 18:27:45 +0300

    test add enableCors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: 'http://localhost:3001' });
   await app.listen(3001);
   // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
   //   AppModule,
