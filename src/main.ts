88b54d24e8a35dd7749f6091215e043464b0e87e
Alexandr2105 <5030553@gmail.com>
2023-07-21 13:51:37 +0300

    test update main1

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
-    options: {
-      port: 3001,
-    },
   });
 
   await app.startAllMicroservices();
