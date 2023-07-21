b318427cd2b7ef5fd541b3c8bba891189c4740c0
Alexandr2105 <5030553@gmail.com>
2023-07-21 15:56:18 +0300

    test update main9

--- src/main.ts
+++ src/main.ts
@@ -3,28 +3,15 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
-    options: { port: 3001 },
-  });
-
-  await app.startAllMicroservices();
-  await app.listen(3001);
-  console.log(`Application is running on: ${await app.getUrl()}`);
-
   // const app = await NestFactory.create(AppModule);
   // await app.listen(3001);
 
-  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-  //   AppModule,
-  //   {
-  //     transport: Transport.TCP,
-  //     options: {
-  //       port: 3001,
-  //     },
-  //   },
-  // );
-  // await app.listen();
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+    },
+  );
+  await app.listen();
 }
 bootstrap();
