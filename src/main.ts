54f3e2c3bc92d5d3ec08d6ce74a5a9c04318d326
Alexandr2105 <5030553@gmail.com>
2023-07-21 14:25:06 +0300

    test update main6

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.startAllMicroservices();
-  await app.listen(3001);
-  // const app = await NestFactory.create(AppModule);
-  // app.connectMicroservice<MicroserviceOptions>({
-  //   transport: Transport.TCP,
-  //   options: { port: 3001, host: 'localhost' },
-  // });
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: { port: 3001, host: 'localhost' },
+  });
   //
   // await app.startAllMicroservices();
   // await app.listen(3001);
