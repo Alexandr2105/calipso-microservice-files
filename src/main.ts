84cdb3552aed92541b5b41b22f4f1fac718a8da0
Alexandr2105 <5030553@gmail.com>
2023-07-22 12:59:43 +0300

    add rabbitmq 6

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,16 @@ async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   // await app.listen(3001);
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-      options: {
-        port: 3001,
-      },
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      port: 3002,
     },
-  );
+  });
 
-  await app.listen();
+  await app.startAllMicroservices();
+  await app.listen(3001);
 
   // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
   //   AppModule,
