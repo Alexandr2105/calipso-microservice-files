11aac94d771f7a44646c98fd00bfd38afa6df0f3
Alexandr2105 <5030553@gmail.com>
2023-07-22 12:29:31 +0300

    add rabbitmq 1

--- src/main.ts
+++ src/main.ts
@@ -6,29 +6,31 @@ async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   // await app.listen(3001);
 
-  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-  //   AppModule,
-  //   {
-  //     transport: Transport.TCP,
-  //   },
-  // );
-  // await app.listen();
-
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.RMQ,
-      options: {
-        urls: [
-          'amqps://nvvffhzg:kunlrWhEIXXBPudNmmJTPT20KOCf8-80@stingray.rmq.cloudamqp.com/nvvffhzg',
-        ],
-        queue: 'FILES_SERVICE',
-        queueOptions: {
-          durable: false,
-        },
-      },
+      transport: Transport.TCP,
+      options: { port: 80 },
     },
   );
+
   await app.listen();
+
+  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  //   AppModule,
+  //   {
+  //     transport: Transport.RMQ,
+  //     options: {
+  //       urls: [
+  //         'amqps://nvvffhzg:kunlrWhEIXXBPudNmmJTPT20KOCf8-80@stingray.rmq.cloudamqp.com/nvvffhzg',
+  //       ],
+  //       queue: 'FILES_SERVICE',
+  //       queueOptions: {
+  //         durable: false,
+  //       },
+  //     },
+  //   },
+  // );
+  // await app.listen();
 }
 bootstrap();
