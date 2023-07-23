05cc3df9d6de54abbdb8e34b60d95222e9950d0e
Alexandr2105 <5030553@gmail.com>
2023-07-23 11:09:48 +0300

    update app

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,16 @@ async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   // await app.listen(3001);
 
-  const app = await NestFactory.create(AppModule);
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
-    options: {
-      port: 3002,
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3001,
+      },
     },
-  });
-
-  await app.startAllMicroservices();
-  await app.listen(3001);
+  );
+  await app.listen();
 
   // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
   //   AppModule,
