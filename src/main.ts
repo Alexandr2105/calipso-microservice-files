a18645cee478dd8318739332917e8e64fae1fc26
Alexandr2105 <5030553@gmail.com>
2023-07-24 12:07:34 +0300

    update port and add transport port 3002

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
+    options: { port: 3002 },
   });
   await app.startAllMicroservices();
-  await app.listen(3000, () => {
+  await app.listen(3001, () => {
     console.log('Started 3001 port');
   });
 
