d160fe3dd6d4442a5f78d3cc20bfd17337c2d085
Alexandr2105 <5030553@gmail.com>
2023-07-24 10:10:05 +0300

    add http

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,25 @@ async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   // await app.listen(3001);
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-      options: {
-        port: 3001,
-      },
-    },
-  );
-  await app.listen();
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+  });
+  await app.startAllMicroservices();
+  await app.listen(3001, () => {
+    console.log('Started 3001 port');
+  });
+
+  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  //   AppModule,
+  //   {
+  //     transport: Transport.TCP,
+  //     options: {
+  //       port: 3001,
+  //     },
+  //   },
+  // );
+  // await app.listen();
 
   // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
   //   AppModule,
