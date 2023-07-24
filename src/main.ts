3d0f158471c2902986420c64c941fbfe28b7ee32
Alexandr2105 <5030553@gmail.com>
2023-07-24 17:07:32 +0300

    update tcp port 2

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
-    options: { port: 3002 },
+    options: { port: 3000 },
   });
   await app.startAllMicroservices();
   await app.listen(3001, () => {
