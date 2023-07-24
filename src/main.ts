6df8764eaac8060a292f7fbfd8656c14c0a0969e
Alexandr2105 <5030553@gmail.com>
2023-07-24 12:22:02 +0300

    update app

--- src/main.ts
+++ src/main.ts
@@ -6,26 +6,26 @@ async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   // await app.listen(3001);
 
-  const app = await NestFactory.create(AppModule);
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
-    options: { port: 3002 },
-  });
-  await app.startAllMicroservices();
-  await app.listen(3001, () => {
-    console.log('Started 3001 port');
-  });
+  // const app = await NestFactory.create(AppModule);
+  // app.connectMicroservice<MicroserviceOptions>({
+  //   transport: Transport.TCP,
+  //   options: { port: 3002 },
+  // });
+  // await app.startAllMicroservices();
+  // await app.listen(3001, () => {
+  //   console.log('Started 3001 port');
+  // });
 
-  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-  //   AppModule,
-  //   {
-  //     transport: Transport.TCP,
-  //     options: {
-  //       port: 3001,
-  //     },
-  //   },
-  // );
-  // await app.listen();
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3002,
+      },
+    },
+  );
+  await app.listen();
 
   // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
   //   AppModule,
