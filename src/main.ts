d48ec82ff259d539e072eaa8f47c19e7bc4c94d0
Alexandr2105 <5030553@gmail.com>
2023-07-24 14:06:06 +0300

    update add http

--- src/main.ts
+++ src/main.ts
@@ -27,21 +27,37 @@ async function bootstrap() {
   // );
   // await app.listen();
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: [
-          'amqps://nvvffhzg:kunlrWhEIXXBPudNmmJTPT20KOCf8-80@stingray.rmq.cloudamqp.com/nvvffhzg',
-        ],
-        queue: 'FILES_SERVICE',
-        queueOptions: {
-          durable: false,
-        },
+  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  //   AppModule,
+  //   {
+  //     transport: Transport.RMQ,
+  //     options: {
+  //       urls: [
+  //         'amqps://nvvffhzg:kunlrWhEIXXBPudNmmJTPT20KOCf8-80@stingray.rmq.cloudamqp.com/nvvffhzg',
+  //       ],
+  //       queue: 'FILES_SERVICE',
+  //       queueOptions: {
+  //         durable: false,
+  //       },
+  //     },
+  //   },
+  // );
+  // await app.listen();
+
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [
+        'amqps://nvvffhzg:kunlrWhEIXXBPudNmmJTPT20KOCf8-80@stingray.rmq.cloudamqp.com/nvvffhzg',
+      ],
+      queue: 'FILES_SERVICE',
+      queueOptions: {
+        durable: false,
       },
     },
-  );
-  await app.listen();
+  });
+  await app.startAllMicroservices();
+  await app.listen(3001);
 }
 bootstrap();
