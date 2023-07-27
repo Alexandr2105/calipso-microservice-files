5ed681b8db56102d82bf44437becf612f75f478d
Alexandr2105 <5030553@gmail.com>
2023-07-27 14:50:28 +0300

    add rabbit

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,15 @@ async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   // await app.listen(3001);
 
-  const app = await NestFactory.create(AppModule);
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
-    options: { port: 3001, host: '0.0.0.0' },
-  });
-  await app.startAllMicroservices();
-  await app.listen(process.env.PORT || 3002, () => {
-    console.log('Started');
-  });
+  // const app = await NestFactory.create(AppModule);
+  // app.connectMicroservice<MicroserviceOptions>({
+  //   transport: Transport.TCP,
+  //   options: { port: 3001, host: '0.0.0.0' },
+  // });
+  // await app.startAllMicroservices();
+  // await app.listen(process.env.PORT || 3002, () => {
+  //   console.log('Started');
+  // });
 
   // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
   //   AppModule,
@@ -26,22 +26,22 @@ async function bootstrap() {
   // );
   // await app.listen();
 
-  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-  //   AppModule,
-  //   {
-  //     transport: Transport.RMQ,
-  //     options: {
-  //       urls: [
-  //         'amqps://nvvffhzg:kunlrWhEIXXBPudNmmJTPT20KOCf8-80@stingray.rmq.cloudamqp.com/nvvffhzg',
-  //       ],
-  //       queue: 'FILES_SERVICE',
-  //       queueOptions: {
-  //         durable: false,
-  //       },
-  //     },
-  //   },
-  // );
-  // await app.listen();
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [
+          'amqps://nvvffhzg:kunlrWhEIXXBPudNmmJTPT20KOCf8-80@stingray.rmq.cloudamqp.com/nvvffhzg',
+        ],
+        queue: 'FILES_SERVICE',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+  await app.listen();
 
   // const app = await NestFactory.create(AppModule);
   // app.connectMicroservice<MicroserviceOptions>({
