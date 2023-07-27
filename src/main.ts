d6af311ba04c6beb199e1e50a9a909a5ee1df501
Alexandr2105 <5030553@gmail.com>
2023-07-27 14:52:37 +0300

    add rabbit and http

--- src/main.ts
+++ src/main.ts
@@ -26,39 +26,39 @@ async function bootstrap() {
   // );
   // await app.listen();
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: [
-          'amqps://nvvffhzg:kunlrWhEIXXBPudNmmJTPT20KOCf8-80@stingray.rmq.cloudamqp.com/nvvffhzg',
-        ],
-        queue: 'FILES_SERVICE',
-        queueOptions: {
-          durable: false,
-        },
-      },
-    },
-  );
-  await app.listen();
-
-  // const app = await NestFactory.create(AppModule);
-  // app.connectMicroservice<MicroserviceOptions>({
-  //   transport: Transport.RMQ,
-  //   options: {
-  //     urls: [
-  //       'amqps://nvvffhzg:kunlrWhEIXXBPudNmmJTPT20KOCf8-80@stingray.rmq.cloudamqp.com/nvvffhzg',
-  //     ],
-  //     queue: 'FILES_SERVICE',
-  //     queueOptions: {
-  //       durable: false,
+  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  //   AppModule,
+  //   {
+  //     transport: Transport.RMQ,
+  //     options: {
+  //       urls: [
+  //         'amqps://nvvffhzg:kunlrWhEIXXBPudNmmJTPT20KOCf8-80@stingray.rmq.cloudamqp.com/nvvffhzg',
+  //       ],
+  //       queue: 'FILES_SERVICE',
+  //       queueOptions: {
+  //         durable: false,
+  //       },
   //     },
   //   },
-  // });
-  // await app.startAllMicroservices();
-  // await app.listen(3001, () => {
-  //   console.log('Started 3001');
-  // });
+  // );
+  // await app.listen();
+
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [
+        'amqps://nvvffhzg:kunlrWhEIXXBPudNmmJTPT20KOCf8-80@stingray.rmq.cloudamqp.com/nvvffhzg',
+      ],
+      queue: 'FILES_SERVICE',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+  await app.startAllMicroservices();
+  await app.listen(process.env.PORT, () => {
+    console.log('Started 3001');
+  });
 }
 bootstrap();
